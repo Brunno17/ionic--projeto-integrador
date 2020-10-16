c916e1a700633ce2476f065624638a24911d3010
brunno marques <brunno.oliveiralm.icloud.com>
2020-10-16 16:49:44 -0300

    Aula Config Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase : {
-  apiKey: "AIzaSyD-a5_iWdNDRTHBo81MP0ukiMlCTjkbQbI",
-  authDomain: "senac-20195.firebaseapp.com",
-  databaseURL: "https://senac-20195.firebaseio.com",
-  projectId: "senac-20195",
-  storageBucket: "senac-20195.appspot.com",
-  messagingSenderId: "604562411891",
-  appId: "1:604562411891:web:32865d9dcb1fbe2d3ff506"
+    apiKey: "AIzaSyAusyGeScaSOLANOKYUY-i9rw8sEckEPQk",
+    authDomain: "projeto-integrador-f5c2f.firebaseapp.com",
+    databaseURL: "https://projeto-integrador-f5c2f.firebaseio.com",
+    projectId: "projeto-integrador-f5c2f",
+    storageBucket: "projeto-integrador-f5c2f.appspot.com",
+    messagingSenderId: "805513023867",
+    appId: "1:805513023867:web:e53ae9ab4834151b922dde"
   }
   };
